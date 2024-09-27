5210e973c5fa714adcf81ce31850284c9a8220ef
ThoriqHanif <thoriqmuh123.alkapro@gmail.com>
2024-09-27 14:09:03 +0700

    Update Title

--- layout/header.php
+++ layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Smartholiday</title>
+    <title>Smart Production</title>
     <link rel="shortcut icon" href="assets/img/logo/logo.svg" type="image/x-icon" />
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
